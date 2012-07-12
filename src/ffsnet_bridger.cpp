4cefbf4656428db297eb613c4cf2b1f03f902875
Pedro Alvarez-Tabio <paltat@gmail.com>
2012-07-12 17:18:01 -0500

    Premerge

--- src/ffsnet_bridger.cpp
+++ src/ffsnet_bridger.cpp
@@ -20,7 +20,6 @@ extern "C" {
 	  return ffs_recvfile(proto, remote_ip, server_port, remote_filename, local_filename);
   }
 
-
   int ffs_sendfile_c(const char *proto, const char *remote_ip, const char *server_port, const char *local_filename, const char *remote_filename) {
 	  return ffs_sendfile(proto, remote_ip, server_port, local_filename, remote_filename);
   }
