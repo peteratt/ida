5e1aab736909ed3c5d20941a367dcd7a5e25ce0c
Pedro Alvarez-Tabio <paltat@gmail.com>
2012-07-26 14:24:12 -0500

    Fixed changed references 3

--- src/zht_bridger.cpp
+++ src/zht_bridger.cpp
@@ -37,7 +37,7 @@ struct metadata* zht_unparse_meta(const char* text) {
 	
 	struct metadata* meta;
 	
-	meta->filename = package.virtualpath();
+	meta->filename = package.virtualpath().c_str();
 	meta->k = package.k();
 	meta->m = package.m();
 	meta->bufsize = package.bufsize();
