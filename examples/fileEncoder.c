4e8592c478d5b6f3b85703a2bb71c3f768162f33
Pedro Alvarez-Tabio <paltat@gmail.com>
2012-07-26 15:36:23 -0500

    Finished: Fixed changed references

---
+++ examples/fileEncoder.c
@@ -0,0 +1,30 @@
+/* Author:  Corentin Debains
+ * Email:   cdebains@iit.edu
+ *
+ * Author:  Pedro Alvarez-Tabio
+ * Email:   palvare3@iit.edu
+ *
+ * File Encoder Example for Erasure-Coding Libraries
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <ffsnet_bridger.h>
+#include <ec.h>
+
+int main(int argc, char* argv[]) {
+
+	//INPUTS: Filename to Encode, # data blocks (n), # parity blocks (m), size of buffer (in B)
+	char * filename = argv[1];
+	int k = atoi(argv[2]);
+	int m = atoi(argv[3]);
+	int bufsize = atoi(argv[4]);
+	
+	ecFileEncode(filename, k, m, bufsize, GIBRALTAR);
+	ecInsertMetadata("neighbors", "config");
+	return ecFileSend(filename, k, m);
+}
