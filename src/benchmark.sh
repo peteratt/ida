3a479e2e0ec8a6dd51dd37bc332dd6fb48db09d5
peteratt <paltat@gmail.com>
2012-07-31 19:02:56 -0500

    Fixed bug in results namespace

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -13,36 +13,36 @@ sleep 1
 
 cd ../examples
 
-if [[ -f results_send_$k_$m_$bufsize.csv ]]; then
-	rm results_send_$k_$m_$bufsize.csv
+if [[ -f results_send.$k.$m.$bufsize.csv ]]; then
+	rm results_send.$k.$m.$bufsize.csv
 fi
-touch results_send_$k_$m_$bufsize.csv
+touch results_send.$k.$m.$bufsize.csv
 
-echo filename,filesize,thEncoding,thSending,thTotal >> results_send_$k_$m_$bufsize.csv #First line of the CSV
+echo filename,filesize,thEncoding,thSending,thTotal >> results_send.$k.$m.$bufsize.csv #First line of the CSV
 
 for file in ida/*
 do
     if [[ -f $file ]]; then
     	echo $file
-        ./fileSenderTEST $file $k $m $bufsize >> results_send_$k_$m_$bufsize.csv
+        ./fileSenderTEST $file $k $m $bufsize >> results_send.$k.$m.$bufsize.csv
     fi
 done
 
 ### RECEIVE ###
 
-if [[ -f results_recv_$k_$m_$bufsize.csv ]]; then
-	rm results_recv_$k_$m_$bufsize.csv
+if [[ -f results_recv.$k.$m.$bufsize.csv ]]; then
+	rm results_recv.$k.$m.$bufsize.csv
 fi
 
 cd ../examples
-touch results_recv_$k_$m_$bufsize.csv
+touch results_recv.$k.$m.$bufsize.csv
 
-echo filename,filesize,thEncoding,thSending,thTotal >> results_recv_$k_$m_$bufsize.csv #First line of the CSV
+echo filename,filesize,thEncoding,thSending,thTotal >> results_recv.$k.$m.$bufsize.csv #First line of the CSV
 
 for file in ida/*
 do
     if [[ -f $file ]]; then
         echo $file
-        #./fileReceiverTEST $file >> results_recv_$k_$m_$bufsize.csv
+        #./fileReceiverTEST $file >> results_recv.$k.$m.$bufsize.csv
     fi
 done
