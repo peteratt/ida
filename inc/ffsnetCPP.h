5a1c49919998249fc8a8104a191e115de7f370b1
corentone <corentone+github@gmail.com>
2012-08-12 03:02:29 -0500

    back on track after file losses

---
+++ inc/ffsnetCPP.h
@@ -0,0 +1,24 @@
+/* Author:  Corentin Debains
+ * Email:   cdebains@iit.edu
+ *
+ * Author:  Pedro Alvarez-Tabio
+ * Email:   palvare3@iit.edu
+ *
+ */
+#include "ec.h"
+#include <udt.h>
+ 
+struct UDTArray_s{
+	int * indexArray;
+	UDTSOCKET * socks;
+	struct metadata * meta;
+	int operation;
+	int socknumber;
+};
+
+typedef struct UDTArray_s * UDTArray;
+
+int * Transfer_init(UDTArray * SsocksP, struct metadata * meta, int operation);
+int bufferSend(UDTArray Ssocks, int index, unsigned char * buffer);
+int bufferRecv(UDTArray Ssocks, int index, unsigned char * buffer);
+int Transfer_destroy(UDTArray Ssocks);
