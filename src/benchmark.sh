4ea92f0ece84c55dad2de519a637b4598fd063c2
peteratt <paltat@gmail.com>
2012-08-15 13:45:15 -0500

    Improved benchmarking scripts

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -3,9 +3,9 @@
 k=$1
 m=$2
 bufsize=$3
-
-./stopServers.sh
-./startServers.sh 6
+targetDir=/mnt/ssd/idaTestFiles
+resFileSend=results_send_`hostname`.$k.$m.$bufsize.csv
+resFileRecv=results_recv_`hostname`.$k.$m.$bufsize.csv
 
 sleep 1
 
@@ -13,33 +13,32 @@ sleep 1
 
 cd ../examples
 
-if [[ -f results_send.$k.$m.$bufsize.csv ]]; then
-	rm results_send.$k.$m.$bufsize.csv
+if [[ -f $resFileSend ]]; then
+	rm $resFileSend
 fi
-touch results_send.$k.$m.$bufsize.csv
+touch $resFileSend
 
-echo filename,filesize,thEncoding,thSending,thTotal >> results_send.$k.$m.$bufsize.csv #First line of the CSV
+echo filename,filesize,thEncoding,thSending,thTotal >> $resFileSend #First line of the CSV
 
-for file in ida/*
+for file in $targetDir/*
 do
     if [[ -f $file ]]; then
     	echo $file
-        ./fileSenderTEST $file $k $m $bufsize >> results_send.$k.$m.$bufsize.csv
+        ./fileSenderTEST $file $k $m $bufsize >> $resFileSend
     fi
 done
 
 ### RECEIVE ###
 
-if [[ -f results_recv.$k.$m.$bufsize.csv ]]; then
-	rm results_recv.$k.$m.$bufsize.csv
+if [[ -f $resFileRecv ]]; then
+	rm $resFileRecv
 fi
 
-cd ../examples
-touch results_recv.$k.$m.$bufsize.csv
+touch $resFileRecv
 
-echo filename,filesize,thEncoding,thSending,thTotal >> results_recv.$k.$m.$bufsize.csv #First line of the CSV
+echo filename,filesize,thEncoding,thSending,thTotal >> $resFileRecv #First line of the CSV
 
-for file in ida/*
+for file in $targetDir/*
 do
     if [[ -f $file ]]; then
         echo $file
